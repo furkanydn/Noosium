c5f66520b374246cf3d4b5b5395e31cf21e25006
furkanaydin <furkan.aydin@inooster.com>
2022-03-04 10:03:17 +0300

    project name changed

--- NoosiumX/Resources/Log/ITestLog.cs
+++
@@ -1,9 +0,0 @@
-namespace NoosiumX.Resources.Log;
-
-public interface ITestLog
-{
-    void Information(string message);
-    void Warning(string message);
-    void Debug(string message);
-    void Error(string message);
-}
