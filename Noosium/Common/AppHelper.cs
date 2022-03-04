616fc97db4a83b6368b31bb86e0a859858a4f88a
Furkan AYDIN <47810525+furkanydn@users.noreply.github.com>
2022-03-04 09:57:39 +0300

    Old test removed

--- Noosium/Common/AppHelper.cs
+++
@@ -1,8 +0,0 @@
-using System.Configuration;
-
-namespace Noosium.Common;
-
-public static class AppHelper
-{
-    public static string AppBaseUrl => ConfigurationManager.AppSettings["Environment"] ?? string.Empty;
-}
